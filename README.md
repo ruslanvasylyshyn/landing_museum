ff0c68a1dd3effbd7436c94c2e8d486a2b3b0a29
Ruslan Vasylyshyn <r.y.vasylyshyn@gmail.com>
2022-10-14 17:19:29 +0300

    small fixes and change project enviroment

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Museum landing page
-  [Landing page design](https://www.figma.com/file/2vaOzFlsfBwDxjwOY8sANd/%D0%9D%D0%90%D0%9C%D0%A3-(Edit)-(Copy))
 
-  [DEMO LINK](https://ruslanvasylyshyn.github.io/landing_museum/)
+[Landing page design](<https://www.figma.com/file/2vaOzFlsfBwDxjwOY8sANd/%D0%9D%D0%90%D0%9C%D0%A3-(Edit)-(Copy)>)
+
+[DEMO LINK](https://ruslanvasylyshyn.github.io/landing_museum/)
 
 - HTML
 - SCSS
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,11 @@
-'use strict';
-
-document.querySelector('.burger').addEventListener('click', function() {
+document.querySelector('.burger').addEventListener('click', () => {
   document.querySelector('.burger__icon').classList.toggle('active');
   document.querySelector('.burger__menu').classList.toggle('animate');
 });
 
-document.querySelector('.burger__nav-list')
-  .addEventListener('click', function() {
+document
+  .querySelector('.burger__nav-list')
+  .addEventListener('click', () => {
     document.querySelector('.burger__icon').classList.toggle('active');
     document.querySelector('.burger__menu').classList.toggle('animate');
   });
