7502132ddc329156d410f4109d00e0da77f9b2fd
Ruslan Vasylyshyn <r.y.vasylyshyn@gmail.com>
2022-10-14 17:20:32 +0300

    Updates

---
+++ index.8d6f53f8.js
@@ -0,0 +1,2 @@
+document.querySelector(".burger").addEventListener("click",(()=>{document.querySelector(".burger__icon").classList.toggle("active"),document.querySelector(".burger__menu").classList.toggle("animate")})),document.querySelector(".burger__nav-list").addEventListener("click",(()=>{document.querySelector(".burger__icon").classList.toggle("active"),document.querySelector(".burger__menu").classList.toggle("animate")}));
+//# sourceMappingURL=index.8d6f53f8.js.map
